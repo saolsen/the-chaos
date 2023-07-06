85de3e03463a2235c083407800fb2166b1bd54de
steve olsen <steve.olsen@close.com>
2023-07-06 08:16:17 -0500

    ignore target

--- rust-test/target/debug/deps/rust_test-e8aabba94ca5d4bc.d
+++
@@ -1,5 +0,0 @@
-/Users/steve/dev/saolsen/the-chaos/rust-test/target/debug/deps/rust_test-e8aabba94ca5d4bc: src/main.rs
-
-/Users/steve/dev/saolsen/the-chaos/rust-test/target/debug/deps/rust_test-e8aabba94ca5d4bc.d: src/main.rs
-
-src/main.rs:
--- rust-test/target/debug/rust-test.d
+++
@@ -1,1 +0,0 @@
-/Users/steve/dev/saolsen/the-chaos/rust-test/target/debug/rust-test: /Users/steve/dev/saolsen/the-chaos/rust-test/src/main.rs
